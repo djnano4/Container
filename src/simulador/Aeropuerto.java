112c79e1deebe27258549cbf4bb4bdfe4ed94bbb
RAUL ORTIZ PUENTE <raul_bustar@hotmail.com>
2015-03-06 11:02:55 +0100

    Con OpenGL básico.

--- src/simulador/Aeropuerto.java
+++ src/simulador/Aeropuerto.java
@@ -1,16 +1,52 @@
 package simulador;
 
 import java.util.ArrayList;
-import org.lwjgl.Sys;
-import org.lwjgl.glfw.*;
-import org.lwjgl.opengl.*;
- 
+
 import java.nio.ByteBuffer;
- 
-import static org.lwjgl.glfw.Callbacks.*;
-import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryUtil.*;
+import org.lwjgl.Sys;
+import static org.lwjgl.glfw.Callbacks.errorCallbackPrint;
+import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
+import static org.lwjgl.glfw.GLFW.GLFW_RELEASE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowShouldClose;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallback;
+import org.lwjgl.glfw.GLFWvidmode;
+import org.lwjgl.opengl.GL11;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_FALSE;
+import static org.lwjgl.opengl.GL11.GL_PROJECTION;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.GL_TRUE;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glClearColor;
+import static org.lwjgl.opengl.GL11.glColor3f;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMatrixMode;
+import static org.lwjgl.opengl.GL11.glVertex3f;
+import org.lwjgl.opengl.GLContext;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Aeropuerto extends Dibujable{
     private GLFWErrorCallback errorCallback;
@@ -25,10 +61,11 @@ public class Aeropuerto extends Dibujable{
     //constructor, de momento vacío.
     }
     
-    public void run(){
+    //while run, limpiar dibuja, makinaria init.
+    public void run() {
         try {
             init();
-            loop();
+            draw();
             glfwDestroyWindow(window);
             keyCallback.release();
         } finally {
@@ -36,56 +73,72 @@ public class Aeropuerto extends Dibujable{
             errorCallback.release();
         }
     }
-    private void init(){
+
+    private void init() {
         glfwSetErrorCallback(errorCallback = errorCallbackPrint(System.err));
-        if ( glfwInit() != GL11.GL_TRUE )
+        if (glfwInit() != GL11.GL_TRUE) {
             throw new IllegalStateException("Unable to initialize GLFW");
-        glfwDefaultWindowHints(); 
-        glfwWindowHint(GLFW_VISIBLE, GL_FALSE);
-        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
- 
-        int WIDTH = 300;
-        int HEIGHT = 300;
-        
+        }
+        glfwDefaultWindowHints(); // optional, the current window hints are already the default
+        glfwWindowHint(GLFW_VISIBLE, GL_FALSE); // the window will stay hidden after creation
+        glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); // the window will be resizable
+
+        int WIDTH = 600;
+        int HEIGHT = 600;
+
         window = glfwCreateWindow(WIDTH, HEIGHT, "Aeropuerto", NULL, NULL);
-        
-        if ( window == NULL )
+        if (window == NULL) {
             throw new RuntimeException("Failed to create the GLFW window");
+        }
+
         glfwSetKeyCallback(window, keyCallback = new GLFWKeyCallback() {
             @Override
             public void invoke(long window, int key, int scancode, int action, int mods) {
-                if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
-                    glfwSetWindowShouldClose(window, GL_TRUE); 
+                if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+                    glfwSetWindowShouldClose(window, GL_TRUE); // We will detect this in our rendering loop
+                }
             }
         });
+        // Get the resolution of the primary monitor
         ByteBuffer vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+        // Center our window
         glfwSetWindowPos(
-            window,
-            (GLFWvidmode.width(vidmode) - WIDTH) / 2,
-            (GLFWvidmode.height(vidmode) - HEIGHT) / 2
+                window,
+                (GLFWvidmode.width(vidmode) - WIDTH) / 2,
+                (GLFWvidmode.height(vidmode) - HEIGHT) / 2
         );
+        // Make the OpenGL context current
         glfwMakeContextCurrent(window);
+        // Enable v-sync
         glfwSwapInterval(1);
+        // Make the window visible
         glfwShowWindow(window);
     }
-    private void loop(){
-        GLContext.createFromCurrent();//Asociado a un hilo. EL unico q manejo.
-        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);//Fijamos el color.
-        while ( glfwWindowShouldClose(window) == GL_FALSE ) { //MIentras no cierres la pantalla.Limpias continuamente.
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
-            glfwSwapBuffers(window);
-            glfwPollEvents();//Recoge si has exo algo en el raton o teclado.
-        }
-    }
-    
+ 
     @Override//Indicamos que vamos a redifinir en esta clase hija un metodo que
     //tiene la clase padre. Cuando se llame a estos metodos, las clases hijas
     //ejecutaran el metodo redefinido en estas y en las que no se hayan redefinido
     //se ejecutara el de la clase padre.
     public void draw() 
     {
-       System.out.println("Dibujando Aeropuerto... "); 
-       run();
+        GLContext.createFromCurrent();
+        glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+ 
+        while (glfwWindowShouldClose(window) == GL_FALSE) {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
+            glfwPollEvents();   
+            /* Render triangle */
+            glBegin(GL_TRIANGLES);
+            glColor3f(1f, 0f, 0f);
+            glVertex3f(-0.6f, -0.4f, 0f);
+            glColor3f(0f, 1f, 0f);
+            glVertex3f(0.6f, -0.4f, 0f);
+            glColor3f(0f, 0f, 1f);
+            glVertex3f(0f, 0.6f, 0f);
+            glEnd();
+            
+            glfwSwapBuffers(window); // swap the color buffers
+        }  
     }
     
     public void creador_pista (float pos_x, float pos_y, float pos_z)
@@ -121,8 +174,7 @@ public class Aeropuerto extends Dibujable{
                 outDibujables.add(objDibujables.get(i));
             } 
         }
-        return outDibujables;
-        
+        return outDibujables;    
     }
 
     public void come_alive()
@@ -135,11 +187,10 @@ public class Aeropuerto extends Dibujable{
         
         while(true) {
         //Dibujamos cada uno de los objetos creados:
-        draw();
+        run();
         for (int i = 0; i < objDibujables.size(); i++)
         {
-            objDibujables.get(i).draw();
-            
+            objDibujables.get(i).draw();     
         }
         //Imprimimos aviones y posiciones:
         System.out.println("En el Aeropuerto hay: " + pull_type(2).size());
